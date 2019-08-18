c07f227e53aba3fbc8ae9f303428070e74c46b6f
Nelia Havrylchyk <gavrilchiknelia@gmail.com>
2019-08-18 20:11:28 +0300

    add changes

--- index.js
+++ index.js
@@ -19,69 +19,65 @@ xhr.onload = function() {
            li.innerHTML = `<p>Фильм: ${title}</p><p>Эпизод: ${episode}</p><p>Описание: ${content}</p><div class="visible">${animation}</div><button type="submit" id="${episode}">Список персонажей</button>`;
 
            ol.appendChild(li);
-
         }
        list.appendChild(ol);
-
     }
 };
 xhr.onerror = function() {
     alert("Запрос не удался");
 };
 
-
 document.addEventListener('click', function (event) {
-    const person = new XMLHttpRequest();
-    person.open('GET', 'https://swapi.co/api/films/');
-    person.send();
-    person.onprogress = function() {
-        const animateArr = document.getElementsByClassName('visible');
-        for (let i = 0; i< animateArr.length; i++) {
-            if (event.target.parentNode == animateArr[i].parentNode) {
-                if (person.loaded < person.total) {
-                    animateArr[i].classList.remove('visible');
+    const animateArr = document.getElementsByClassName('visible');
+    event.target.style.display = "none";
+    for (let k = 0; k < animateArr.length; k++) {
+
+        if (event.target.parentNode == animateArr[k].parentNode) {
+            animateArr[k].classList.add('nVis');
+            animateArr[k].classList.remove('visible');
+
+            const person = new XMLHttpRequest();
+            person.open('GET', 'https://swapi.co/api/films/');
+            person.send();
+            person.onload = function () {
+                if (person.status !== 200) {
+                    alert(`Ошибка ${person.status}: ${person.statusText}`);
                 } else {
-                    animateArr[i].classList.add('visible');
-                }
-            }
-        }
-    };
-    person.onload = function() {
-        if (person.status !== 200) {
-            alert(`Ошибка ${person.status}: ${person.statusText}`);
-        } else {
 
-            let ol = document.createElement('ol');
-            let filmPerson = JSON.parse(person.response);
-            console.log(filmPerson);
-            for ( let i = 0; i < filmPerson.results.length; i++) {
-                let per = filmPerson.results[i].characters;
-                if (filmPerson.results[i].episode_id == event.target.id ) {
-                    for (let j = 0; j < per.length; j++) {
-                        let personItem = new XMLHttpRequest();
-                        personItem.open('GET', `${per[j]}`);
-                        personItem.send();
-                        personItem.onload = function() {
-                            // console.log(personItem.response);
-                            let personDetail = JSON.parse(personItem.response);
-                            console.log(personDetail);
-                            let li = document.createElement('li');
-                            li.innerHTML = `<p>Имя: ${personDetail.name}.</p><p>Рост: ${personDetail.height}.</p><p>Вес: ${personDetail.mass}.</p><p>Цвет волос: ${personDetail.hair_color}.</p><p>Цвет кожи: ${personDetail.skin_color}.</p>`;
-                            ol.appendChild(li);
-                        };
-                        personItem.onerror = function() {
-                            alert("Запрос не удался");
-                        };
+                    let ol = document.createElement('ol');
+                    let filmPerson = JSON.parse(person.response);
+                    console.log(filmPerson);
+                    for (let i = 0; i < filmPerson.results.length; i++) {
+                        let per = filmPerson.results[i].characters;
+                        if (filmPerson.results[i].episode_id == event.target.id) {
+                            for (let j = 0; j < per.length; j++) {
+                                let personItem = new XMLHttpRequest();
+                                personItem.open('GET', `${per[j]}`);
+                                personItem.send();
+                                personItem.onload = function () {
+                                    let personDetail = JSON.parse(personItem.response);
+                                    let li = document.createElement('li');
+                                    li.innerHTML = `<p>Имя: ${personDetail.name}.</p><p>Рост: ${personDetail.height}.</p><p>Вес: ${personDetail.mass}.</p><p>Цвет волос: ${personDetail.hair_color}.</p><p>Цвет кожи: ${personDetail.skin_color}.</p>`;
+
+                                    ol.appendChild(li);
+                                };
+                                personItem.onerror = function () {
+                                    alert("Запрос не удался");
+                                };
+                            }
+                            let nVis = document.getElementsByClassName('nVis');
+                            for (let t =0; t < nVis.length; t++)  {
+                                nVis[t].classList.add('visible');
+                                nVis[t].classList.remove('nVis');
+                            }
+                            event.target.parentNode.appendChild(ol);
+                        }
                     }
-                    event.target.parentNode.appendChild(ol);
-                    event.target.style.display = "none";
-
                 }
-            }
-
+            };
+            person.onerror = function () {
+                alert("Запрос не удался");
+            };
         }
-    };
-    person.onerror = function() {
-        alert("Запрос не удался");
-    };
+    }
 });
